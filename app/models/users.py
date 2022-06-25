4ab787f49ea5e1d099eb6b280448d79d4e453435
umschaudhary <umesschaudhary@gmail.com>
2022-06-26 03:02:41 +0545

    add repr on User

--- app/models/users.py
+++ app/models/users.py
@@ -1,20 +1,19 @@
 from uuid import uuid4
+
+from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Date
 from sqlalchemy.orm import relationship
 
 from app.core.db import Base
 
 
 class User(Base):
-    id = Column(
-        UUID(as_uuid=True),
-        primary_key=True,
-        index=True,
-        default=uuid4
-    )
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid4)
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False, index=True)
     password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
+
+    def __repr__(self):
+        return f"{self.email}, {self.username}"
